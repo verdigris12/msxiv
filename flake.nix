13aaed0d6deb4e1a66db91149e5f59d86e7152d8
verdigris <verdigris1903@proton.me>
2025-03-29 01:14:42 +0000

    version bump

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
     packages.${system} = {
       msxiv = pkgs.stdenv.mkDerivation {
         pname = "msxiv";
-        version = "0.1";
+        version = "0.1.1";
         src = ./.;
 
         nativeBuildInputs = [
